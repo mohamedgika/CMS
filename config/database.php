5e20a359dfc13070f4668e0db9f785937e6424f7
mohamedgika <dodm773@gmail.com>
2025-10-23 23:11:39 +0300

    update oop project

--- config/database.php
+++ config/database.php
@@ -1,10 +1,30 @@
 <?php
-function connectionDB()
+
+namespace ConfigDB;
+class Database
 {
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "cms";
-    $connection = mysqli_connect($server, $username, $password, $database);
-    return $connection;
+    private $server = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "cms";
+    private $connection;
+
+    function __construct()
+    {
+        $connection = mysqli_connect($this->server, $this->username, $this->password, $this->database);
+        $this->connection = $connection;
+    }
+
+    public function getAll($table)
+    {
+        $data = mysqli_query($this->connection, "SELECT * FROM $table");
+        $data = mysqli_fetch_all($data);
+        return $data;
+    }
+
+    function __destruct()
+    {
+        mysqli_close($this->connection);
+    }
+
 }
--- index.php
+++ index.php
@@ -1,40 +1,21 @@
 <?php
-include ("config/database.php");
-include ("app/model/user.php");
 
-$connection = connectionDB();
-
-if (!$connection) {
-    echo "Connection failed";
-} else {
-    // echo "Connection successful";
-    //SQL => Structured Query Language
-    // $get_user_table = "SELECT * FROM users";
-    // $result = mysqli_query($connection, $get_user_table);
-    $data = getUsers($connection);
-
-    //Count number of rows
-    $num_of_rows = mysqli_num_rows($result);
-
-
-    if ($num_of_rows > 0) {
-
-        $user_data = [];
-        //Data
-        $data = mysqli_fetch_assoc($result);
-        $user_data[] = $data;
-
-        // while ($data) {
-        //     $user_data[] = $data;
-        // }
-        echo "<pre>";
-        print_r($user_data);
-    } else {
-        echo "No data found";
-    }
+require_once "config/database.php";
+use ConfigDB\Database;
 
 
 
-}
+$database = new Database();
+
+$users = $database->getAll("users");
+echo "<pre>";
+print_r($users);
+
+$categories = $database->getAll("categories");
+echo "<pre>";
+print_r($categories);
+
+
+
 
 ?>
